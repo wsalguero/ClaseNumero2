7a2ea6181c1133f5b45fe7b22d02d54bd398fc87
William Salguero <wsalguero-2021689@kinal.edu.gt>
2023-01-18 14:37:47 -0600

    Version One

---
+++ index.js
@@ -0,0 +1,28 @@
+const valor1 = document.getElementById('valor1').value;
+const valor2 = document.getElementById('valor2').value;
+
+
+const suma = document.getElementById('suma');
+
+function sumar() {
+    let numero1 = document.getElementById("valor1").value
+    let numero2 = document.getElementById("valor2").value
+
+    let suma =parseInt(numero1) + parseInt(numero2);
+
+}
+
+/*
+function getSuma() {
+    //const suma = valor1 + valor2
+    console.log(valor1 + valor2);
+
+
+    //console.log(getSuma(suma));
+    return suma;
+}
+
+function getClick() {
+    console.log(suma)
+}
+*/
